cb676b7aab5a7243509f4499683169b40400bed2
Bharath <reddybharath.rb@gmail.com>
2026-04-08 11:30:18 -0500

    feat: error handling and logging for pipeline

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,13 +1,35 @@
 import schedule 
 import time 
 import subprocess
+import logging
 from datetime import  datetime
-
+logging.basicConfig(
+    filename="logs/pipeline.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 def run_pipeline():
-    print(f"Pipeline started at {(datetime.now())}")
-    subprocess.run(["python", "src/ingestion/fetch_prices.py"])
-    subprocess.run(["python", "src/ingestion/db_loader.py"])
-    subprocess.run(["dbt","run","--project-dir","transforms"])
+    logging.info(f"Pipeline started at {(datetime.now())}")
+
+    result = subprocess.run(["venv\\scripts\\python.exe" , "src/ingestion/fetch_prices.py"])
+    if result.returncode != 0:
+        logging.error("fetch_prices.py failed. Stopping pipeline.")
+        return
+    logging.info("fetch_prices.py completed successfully.")
+
+    result = subprocess.run(["venv\\scripts\\python.exe", "src/ingestion/db_loader.py"])
+    if result.returncode!=0:
+        logging.error("db_loader.py is failed , pipeline stopped ")
+        return
+    logging.info("dbt_loader.py completed successfully")
+    
+    result = subprocess.run(["dbt","run","--project-dir","transforms"])
+    if result.returncode!=0:
+        logging.error("dbt failed , stopping pipeline ")
+        return
+    logging.info("dbt runned successfully")
+    
+logging.info(f"Pipeline completed successfully at {datetime.now()}")
     
 schedule.every().day.at ("06:00").do (run_pipeline)
 print ("scheduler running . pipeline everyday at 06:00.")
